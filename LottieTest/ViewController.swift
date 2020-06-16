984e5e393c3c92c9451601b3e2984a19e829ebe4
Daniel Hjartstrom <daniel.hjartstrom@consid.se>
2020-06-16 14:21:55 +0200

    Initial Commit

---
+++ LottieTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieTest
+//
+//  Created by Daniel Hjärtström on 2020-06-16.
+//  Copyright © 2020 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
